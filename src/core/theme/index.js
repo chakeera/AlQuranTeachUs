dc6c013e2f4e55fe015dc2b5e8ce014a2568376c
Chakeera <chakeera1999@gmail.com>
2022-02-01 12:23:37 +0400

    linting and prettierrc

---
+++ src/core/theme/index.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#45503B",
+      cream: "#FAEDCD",
+      whitewash: "#FEFAE0",
+      brown: "#D4A373",
+      lightgreen: "#CCD5AE",
+    },
+    secondary: {
+      main: "#CCD5AE",
+    },
+  },
+});
+
+export default theme;
