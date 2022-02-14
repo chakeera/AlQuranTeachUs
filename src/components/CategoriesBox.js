57e14ce1ea3fc91bd5a74de6561a9092f91b18a3
Chakeera <chakeera1999@gmail.com>
2022-02-14 18:22:47 +0400

    Add calendar

--- src/components/CategoriesBox.js
+++ src/components/CategoriesBox.js
@@ -1,10 +1,14 @@
-import { Stack, Divider, Grid, Card, CardMedia, CardContent, Typography } from '@mui/material';
+import { Stack, Divider, Grid, Card, CardMedia, Typography } from '@mui/material';
 import React from 'react';
+import { CalendarComponent, Inject, Islamic } from '@syncfusion/ej2-react-calendars';
+import '../style/calendar.css';
 import QuranPhoto from '../assets/images/quran.jpg';
 import TafseerPhoto from '../assets/images/tafseer.jpg';
-import DuaPhoto from '../assets/images/dua.jpeg';
-import KhutbahPhoto from '../assets/images/khutbah.jpg';
-import TalkPhoto from '../assets/images/talk.png';
+import FridayPhoto from '../assets/images/friday.jpg';
+import FacebookPhoto from '../assets/images/facebook.jpeg';
+import YoutubePhoto from '../assets/images/youtube.jpg';
+import OtherPhoto from '../assets/images/other.png';
+import { Box } from '@mui/system';
 
 const CategoriesBox = () => {
   return (
@@ -17,53 +21,48 @@ const CategoriesBox = () => {
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
           <Grid item xs={6} md={4}>
             <Card sx={{ mb: 1, mt: 2 }}>
-              <CardMedia component="img" width="100" image={QuranPhoto} alt="Quran" />
+              <CardMedia component="img" image={FridayPhoto} alt="Friday" />
             </Card>
-            <Typography align="center">กุรอาน</Typography>
+            <Typography align="center">คุตบะห์วันศุกร์ตัฟซีรอัลกุรอาน</Typography>
           </Grid>
           <Grid item xs={6} md={4}>
             <Card sx={{ mb: 1, mt: 2 }}>
-              <CardMedia component="img" width="100" image={TafseerPhoto} alt="Tafseer" />
+              <CardMedia component="img" image={TafseerPhoto} alt="Tafseer" />
             </Card>
-            <Typography align="center">ตัฟซีร</Typography>
+            <Typography align="center">บรรยาย ตัฟซีรอัลกุรอาน</Typography>
           </Grid>
           <Grid item xs={6} md={4}>
             <Card sx={{ mb: 1, mt: 2 }}>
-              <CardMedia component="img" width="100" image={KhutbahPhoto} alt="Khutbah" />
+              <CardMedia component="img" image={FacebookPhoto} alt="Khutbah" />
             </Card>
-            <Typography align="center">คุตบะหวันศุกร์</Typography>
+            <Typography align="center">Facebook ตัฟซีรอัลกุรอาน</Typography>
           </Grid>
           <Grid item xs={6} md={4}>
             <Card sx={{ mb: 1, mt: 2 }}>
-              <CardMedia component="img" width="100" image={TalkPhoto} alt="บรรยาย" />
+              <CardMedia component="img" image={YoutubePhoto} alt="บรรยาย" />
             </Card>
-            <Typography align="center">บรรยาย</Typography>
+            <Typography align="center">YouTube ตัฟซีรอัลกุรอาน</Typography>
           </Grid>
           <Grid item xs={6} md={4}>
             <Card sx={{ mb: 1, mt: 2 }}>
-              <CardMedia component="img" width="100" image={DuaPhoto} alt="Dua" />
+              <CardMedia component="img" image={QuranPhoto} alt="Dua" />
             </Card>
-            <Typography align="center">ดุอาจากกุรอาน</Typography>
+            <Typography align="center">หนังสืออัลกุรอาน & MP3 อัลกุรอานเพราะๆ</Typography>
           </Grid>
           <Grid item xs={6} md={4}>
             <Card sx={{ mb: 1, mt: 2 }}>
-              <CardMedia component="img" width="100" alt="Download" />
+              <CardMedia component="img" image={OtherPhoto} alt="Download" />
             </Card>
-            <Typography align="center">คุตบะหวันศุกร์</Typography>
+            <Typography align="center">หลักฐานอื่นๆ</Typography>
           </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <Card sx={{ mb: 2, mt: 2, maxWidth: 400 }}>
-            <CardContent>
-              <Typography gutterBottom variant="h6" component="div">
-                8 sentence เนื้อหาcontentของtopicนี้ใส่ได้ไม่เกิน5บรรทัดนี้ ใส่ได้ไม่เกิน5บรรทัดนี้
-                ใส่ได้ไม่เกิน5บรรทัดนี้ ใส่ได้ไม่เกิน5บรรทัดนี้ ใส่ได้ไม่เกิน5บรรทัด
-                ใส่ได้ไม่เกิน5บรรทัดกินกี่ เนื้อหาcontentของtopicนี้ใส่ได้ไม่เกิน5บรรทัดนี้
-                ใส่ได้ไม่เกิน5บรรทัดนี้ ใส่ได้ไม่เกิน5บรรทัดนี้ ใส่ได้ไม่เกิน5บรรทัดนี้
-                ใส่ได้ไม่เกิน5บรรทัด ใส่ได้ไม่เกิน5บรรทัดกินกี่
-              </Typography>
-            </CardContent>
-          </Card>
+        <Grid item xs={5}>
+          <Box sx={{ my: 2 }}>
+            <CalendarComponent id="calendar" calendarMode="Islamic">
+              <Inject services={[Islamic]} />
+            </CalendarComponent>
+          </Box>
+          <CalendarComponent id="calendar"></CalendarComponent>
         </Grid>
       </Stack>
     </div>
