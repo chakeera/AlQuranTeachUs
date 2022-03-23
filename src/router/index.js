90ccab33e7b7ab8d36b9b5981fa88448ae7b3f6e
Chakeera <chakeera1999@gmail.com>
2022-03-23 20:04:05 +0400

    failed design

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home.js';
+// import Home from '../pages/Home.js';
+import NewHome from '../pages/NewHome.js';
 import Jummah from '../pages/Jummah.js';
 import Khutbah from '../pages/Khutbah.js';
 import Manage from '../pages/Manage.js';
@@ -9,7 +10,8 @@ import DisplayFolder from '../components/ContentPage/DisplayFolders';
 const router = () => (
   <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={<Home />} />
+      {/* <Route exact path="/" element={<Home />} /> */}
+      <Route exact path="/" element={<NewHome />} />
       <Route path="/Jummah" element={<Jummah />} />
       <Route path="/Khutbah" element={<Khutbah />} />
       <Route path="/Khutbah/:scholar" element={<DisplayFolder category="Khutbah" />} />
